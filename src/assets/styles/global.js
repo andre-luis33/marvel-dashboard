243ce42cb71063889c7f22a287c2c9be3ddc4211
André Luís <andreluismoura22@gmail.com>
2023-07-27 09:58:39 -0300

    first commit

---
+++ src/assets/styles/global.js
@@ -0,0 +1,39 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+   * {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: sans-serif;
+	}
+
+   img {
+      display: block;
+      max-width: 100%;
+   }
+
+   a {
+      text-decoration: none;
+   }
+
+   body {
+      background-color: ${({ theme }) => theme.colors.backgroundColor };
+   }
+
+   button {
+      cursor: pointer;
+   }
+
+   button[disabled] {
+      cursor: not-allowed;
+   }
+`;
+
+export const Container = styled.div`
+   margin: 0 auto;
+   padding: 0 10px;
+   max-width: 700px;
+
+   position: relative;
+`;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,23 @@
+export default {
+
+	colors: {
+		white: '#ffffff',
+		black: '#000000',
+		divider: '#ebeff2',
+		blue: {
+			200: '#747d94',
+			600: '#213770',
+			800: '#00113d',
+		},
+		orange: {
+			400: '#f43724',
+			500: '#f21a05'
+		},
+		gray: {
+			background: '#f5f6f8',
+			100: '#eaecf0',
+			500: '#777777'
+		}
+	}
+
+};
