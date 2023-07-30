0ad4e382078cd551a81267da0683f490a455e652
André Luís <andreluismoura22@gmail.com>
2023-07-30 17:55:07 -0300

    add: started home page development

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 export default function Router() {
 	return (
 		<Routes>
 			<Route path='/login' Component={Login} exact />
+			<Route path='/home' Component={Home} exact />
 		</Routes>
 	);
 }
